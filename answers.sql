6586acaf11fe6b70d1599b63b08e6f201e2a4008
Tara Mehryar <tara.mehryar@gmail.com>
2023-09-14 18:42:27 -0500

    completed problem 4

--- answers.sql
+++ answers.sql
@@ -18,6 +18,14 @@ WHERE customer_id IN (
     WHERE id = 1
 );
 
+SELECT id
+FROM orders
+WHERE customer_id = (
+    SELECT id
+    FROM customers
+    WHERE id = 1
+);
+
 -- 3. Employees need to be scheduled for next week based on how many cupcakes have been ordered and not yet processed.
 -- Write a query that gets the total number of cupcakes from unprocessed orders.
 SELECT SUM(num_cupcakes)
@@ -28,13 +36,29 @@ WHERE processed = 'f';
 -- Write a query that shows the name of each cupcake and the sum of cupcakes ordered for that cupcake type 
 -- (for both processed and unprocessed orders), sorted in ascending alphabetical order of cupcake name.
 -- The report should show all cupcake types, even if they have not been ordered at all.
+SELECT c.name, COALESCE(SUM(o.num_cupcakes), 0) AS sum
+FROM cupcakes c
+LEFT JOIN orders o ON c.id = o.cupcake_id
+group BY c.name
+order BY c.name ASC
+
 
 -- 5. Marketing now wants to send an email to its top customers, thanking them for their business and offering them a special deal for next month.
 -- Write a query that shows the email address of each customer and the total number of cupcakes they’ve ordered. 
 -- Results should be sorted by total number of cupcakes, in descending order.
+SELECT *
+FROM customers
+GROUP BY (
+    SELECT SUM(num_cupcakes)
+    FROM orders
+)
+ORDER BY descending;
+
 
 -- 6. Uh oh — it looks like there might have been an issue with a recent batch of funfetti cupcakes 
 -- (someone might have ordered non-edible sparkles to put in the icing instead of edible ones). 
 -- We need to let our customers who have processed orders of funfetti cupcakes know about the issue.
 -- Write a query that selects the first name, last name and email address of customers who have processed orders of funfetti cupcakes. 
 -- Even if a customer has multiple outstanding orders of funfetti, their email should only appear once.
+
+
