d3f9e88e871a49df70c5ad9a76ee8f32be6e8a7d
Tara Mehryar <tara.mehryar@gmail.com>
2023-09-14 14:15:53 -0500

    completed problem 1

--- answers.sql
+++ answers.sql
@@ -3,6 +3,8 @@
 -- 1. The Wasatch Cupcake Shop is getting ready to send a marketing email announcing a new promotion on Funfetti cupcakes. 
 -- We need to get all of our customers’ email addresses to Marketing.
 -- Select all of the email addresses from the customers table in alphabetical order.
+SELECT customers.email
+FROM customers;
 
 -- 2. Elizabeth Crocker has requested receipts for all of her past cupcake orders. 
 -- To look up the receipts, we need to know which orders Elizabeth placed.
